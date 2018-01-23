2abdba21e06062374573c7536270537a52c28aa1
Vijay Sharma <vijay.sharm@gmail.com>
2018-01-22 20:22:14 -0500

    deckle

---
+++ spec/rw-deckle-spec.js
@@ -0,0 +1,18 @@
+'use babel';
+
+import RwDeckle from '../lib/rw-deckle';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('RwDeckle', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('rw-deckle');
+  });
+  
+});
